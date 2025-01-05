9d85c0b59c612117b1c08a806e0831c61da44c0d
Michael Wertheim <michaelwerthein@michaels-mbp-2.mynetworksettings.com>
2025-01-05 05:50:26 -0500

    Fix PostCSS configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    open: true,
-    strictPort: true,
-    cors: true
-  },
-  preview: {
-    port: 5173,
-    open: true
+  build: {
+    outDir: 'dist'
   }
 })
